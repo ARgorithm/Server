c1fb7a93adf70c758d6ca84e1fd13904db149968
Alan John <alanpjohn@outlook.com>
2020-10-19 22:54:13 +0530

    fixed metadata error

--- app/app/model/argorithm.py
+++ app/app/model/argorithm.py
@@ -14,7 +14,7 @@ def allowed_file(filename):
 class Argorithm:
 
     def __init__(self,**kwargs):
-        self.name = kwargs['name']
+        self.argorithmID = kwargs['argorithmID']
         self.file = kwargs['filename']
         self.function = kwargs['function']
         self.parameters = kwargs['parameters']
@@ -23,7 +23,7 @@ class Argorithm:
 
     def describe(self):
         return {
-            "name" : self.name,
+            "argorithmID" : self.argorithmID,
             "parameters" : self.parameters,
             "filename" : self.file,
             "function" : self.function,
@@ -32,7 +32,7 @@ class Argorithm:
         }
 
     def __str__(self):
-        return self.name
+        return self.argorithmID
 
     def run_code(self,parameters):
         filepath = "app.uploads." + self.file[:-3]
@@ -51,16 +51,16 @@ class ArgorithmManager():
     def list(self):
         return self.register.list()
 
-    def search(self,name):
-        print(name)
+    def search(self,argorithmID):
+        print(argorithmID)
         try:
-            return Argorithm(**self.register.search(name))
+            return Argorithm(**self.register.search(argorithmID))
         except:
             return None
 
     def insert(self,data,file):
-        if self.search(data['name']) != None:
-            return {"status" : "name already exists"}
+        if self.search(data['argorithmID']) != None:
+            return {"status" : "argorithmID already exists"}
         if allowed_file(file.filename):
             filename = secure_filename(file.filename)
             if filename != data['file']:
@@ -74,21 +74,21 @@ class ArgorithmManager():
                 count+=1
             file.save(os.path.join(UPLOAD_FOLDER, final_filename))
             metadata = Argorithm(
-                name=data['name'],
+                argorithmID=data['argorithmID'],
                 filename=final_filename,
                 function=data['function'],
                 parameters=data['parameters'],
                 description=data['description'],
                 default=data["default"]
             )
-            self.register.insert(key=data['name'] , value= metadata.describe())
+            self.register.insert(key=data['argorithmID'] , value= metadata.describe())
             return {
                 "status" : "successful"
             }
         return {"status" : "not python file"}
 
     def process(self,data):
-        function = self.search(data['function'])
+        function = self.search(data['argorithmID'])
         if function==None:
             return {"status" : "not present"}
         # try:
